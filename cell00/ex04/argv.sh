0fe19582c90b526226e3b8bec65a8f8fa7354c42
phuwishpk <phuwish123@gmail.com>
2026-09-15 02:39:17 +0700

    feat: add bash scripts and HTML pages for cell00 and cell01 exercises

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+else
+    if [ -n "$1" ]; then
+        echo "$1"
+    fi
+    if [ -n "$2" ]; then
+        echo "$2"
+    fi
+    if [ -n "$3" ]; then
+        echo "$3"
+    fi
+fi
